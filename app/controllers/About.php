ca506cffc76a9a6225b2c773e0006dacc8595044
Evi Silvia <evi_173040050@mail.unpas.ac.id>
2019-10-10 19:25:16 +0700

    Menambahkan bootstrap

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -15,7 +15,7 @@ class About extends Controller{
 	 }
 	public function page()
 	 {
-	 	$data['judul'] = 'Pages'
+	 	$data['judul'] = 'Pages';
 	    $this->view('templates/header',$data);
 		$this->view('about/page');
 		$this->view('templates/footer');
